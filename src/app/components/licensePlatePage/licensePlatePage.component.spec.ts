3c7064e7380ed7571fff618ac9b316f09bf5eb0b
Joel Camus Bueno <joelcamus@gmail.com>
2022-11-09 10:31:10 +0100

    Subir dependencias

--- src/app/components/licensePlatePage/licensePlatePage.component.spec.ts
+++ src/app/components/licensePlatePage/licensePlatePage.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync as  } from '@angular/core/testing';
 
 import { LicensePlatePageComponent } from './licensePlatePage.component';
 
@@ -6,7 +6,7 @@ describe('LicensePlatePageComponent', () => {
     let component: LicensePlatePageComponent;
     let fixture: ComponentFixture<LicensePlatePageComponent>;
 
-    beforeEach(async(() => {
+    beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
             declarations: [LicensePlatePageComponent]
         })
--- src/app/components/nifNieCifPage/nifNieCifPage.component.spec.ts
+++ src/app/components/nifNieCifPage/nifNieCifPage.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync as  } from '@angular/core/testing';
 
 import { NifNieCifPageComponent } from './nifNieCifPage.component';
 describe('NifNieCifPageComponent', () => {
   let component: NifNieCifPageComponent;
   let fixture: ComponentFixture<NifNieCifPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [NifNieCifPageComponent]
     })
